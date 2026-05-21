d97ad5c4f29375194a07a0714f0630ba37b2724c
Jemima-Harisoa <jemimarazafimandimby@gmail.com>
2026-05-22 01:17:25 +0300

    fix: add Django Filters configuration to REST framework settings

--- projetFinal/weather_dashboard/settings.py
+++ projetFinal/weather_dashboard/settings.py
@@ -38,8 +38,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
     'weather',
 ]
+
+# Configuration rest_framework
+REST_FRAMEWORK = {
+    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE": 50,
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
